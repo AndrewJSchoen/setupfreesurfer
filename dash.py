c03406f1284bbcd85328f64051560e53f0bd69cd
AndrewJSchoen <schoen.andrewj@gmail.com>
2016-06-01 09:52:59 -0500

    0.1
    [1] Initial Commit

---
+++ dash.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import json
+import shutil
+import string
+import copy
+import re
+from docopt.docopt import docopt
+
+Version = "0.1"
+doc = """
+Dash.
+
+Usage:
+  dash.py create <dir> (-n <text> | --name <text>)
+  dash.py update <dir> [--addrow <row>...] [--rmrow <row>...] [--addcol <col>...] [--rmcol <col>...]
+  dash.py cell <dir> (-r <row> | --row <row>) (-c <col> | --col <col>) [options]
+
+Commands:
+  create           Create an empty dash in the directory specified.
+  update           Update a dash in the directory specified. See options for specifics.
+  cell             Update specific cells by row/column id.
+
+Options:
+  -h --help                                       Show this screen.
+  -v --version                                    Show the current version.
+  -n <text> --name <text>                         Specify the name of the dash. (create)
+  --addrow <row>...                               Add a row. (update)
+  --addcol <col>...                               Add a column. (update)
+  --rmrow <row>...                                Remove a row. (update)
+  --rmcol <col>...                                Remove a column. (update)
+  -r <row> --row <row>                            Specify the row of the cell. (cell)
+  -c <col> --col <col>                            Specify the column of the cell. (cell)
+  --settext <text>                                Specify the new text. (cell)
+  --setbgcolor <hex>                              Specify the new background color. (cell)
+  --settxtcolor <hex>                             Specify the new text color. (cell)
+  --setanimate <animation>                        Specify the animation. Choose from 'wave', 'toggle', 'bars', or 'none'. (cell)
+  --setbool <bool>                                Specify the value of the cell boolean. Choose from 'True', 'False' or 'None'. (cell)
+  --addimage <path>                               Add an image to the cell. (cell)
+  --rmimage <index>                               Remove an image from the cell. (cell)
+  --addnote <text>                                Add a note to the cell. (cell)
+"""
+
+def write_json(path, data):
+    with open(path, 'w') as outfile:
+        json.dump(data, outfile, sort_keys=True, indent=4, ensure_ascii=False)
+
+def update_json(path, callback=None, **kwargs):
+    with open(cleaned_path(path), "r+") as jsonFile:
+        jsondata = json.load(jsonFile)
+        jsonFile.seek(0)
+        jsonFile.truncate()
+        newdata = callback(byteify(jsondata), **kwargs)
+        if newdata != None:
+            json.dump(newdata, jsonFile, sort_keys=True, indent=4, ensure_ascii=False)
+    return newdata
+
+def read_json(path):
+    try:
+        with open(cleaned_path(path), "r") as jsonFile:
+          jsondata = json.load(jsonFile)
+          jsonFile.seek(0)
+        return byteify(jsondata)
+    except:
+        print("Error! json file '{0}' does not exist!".format(cleaned_path(path)))
+        return None
+
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key):byteify(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [ byteify(element) for element in input ]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
+def is_hex_color(string):
+    match = re.search(r'^#(?:[0-9a-fA-F]{1,2}){3}$', string)
+    return match
+
+def idify(name):
+    valid_chars = "-_.{0}{1}".format(string.ascii_letters, string.digits)
+    returnid = ''.join(c for c in name if c in valid_chars)
+    return returnid
+
+def get_dash_src():
+    return os.path.abspath(os.path.join(os.path.dirname( __file__ )))
+
+def cleaned_path(path):
+  if path.endswith("/"):
+    path = path[:-1]
+  if path.startswith("="):
+    path = path[1:]
+  realpath = os.path.realpath(path)
+  return realpath
+
+def structure_updater(structure, root, add_columns=None, remove_columns=None, add_rows=None, remove_rows=None):
+    working = copy.deepcopy(structure)
+    removed_columns = []
+    removed_rows = []
+    added_columns = []
+    added_rows = []
+    if add_columns != None and add_columns != []:
+        for column in add_columns:
+            candidate_column_id = idify(column)
+            if candidate_column_id not in [existingcolumn["id"] for existingcolumn in structure["cols"]]:
+                working["cols"].append({"id":candidate_column_id,"text":str(column)})
+                added_columns.append({"id":candidate_column_id,"text":str(column)})
+    if add_rows != None and add_rows != []:
+        for row in add_rows:
+            candidate_row_id = idify(row)
+            if candidate_row_id not in [existingrow["id"] for existingrow in structure["rows"]]:
+                working["rows"].append({"id":candidate_row_id,"text":str(row)})
+                added_rows.append({"id":candidate_row_id,"text":str(row)})
+    if remove_columns != None and remove_columns != []:
+        for column in remove_columns:
+            if idify(column) in [existingcolumn["id"] for existingcolumn in working["cols"]]:
+                for candidate_remove_column in working["cols"]:
+                    if idify(column) == candidate_remove_column["id"]:
+                        working["cols"].remove(candidate_remove_column)
+                        removed_columns.append(idify(column))
+    if remove_rows != None and remove_rows != []:
+        for row in remove_rows:
+            if idify(row) in [existingrow["id"] for existingrow in working["rows"]]:
+                for candidate_remove_row in working["rows"]:
+                    if idify(row) == candidate_remove_row["id"]:
+                        working["rows"].remove(candidate_remove_row)
+                        removed_rows.append(idify(row))
+    if len(working["rows"]) >= 1 and len(working["cols"]) >= 1:
+        for column in removed_columns:
+            for cell in ["{0}-{1}".format(row["id"], column) for row in structure["rows"]]:
+                try:
+                    os.unlink(root+"/data/"+cell+".json")
+                except:
+                    pass
+        for row in removed_rows:
+            for cell in ["{0}-{1}".format(row, column["id"]) for column in structure["cols"]]:
+                try:
+                    os.unlink(root+"/data/"+cell+".json")
+                except:
+                    pass
+        structure_row_set = set([row["id"] for row in structure["rows"]])
+        structure_col_set = set([col["id"] for col in structure["cols"]])
+        removed_row_set = set(removed_rows)
+        removed_col_set = set(removed_columns)
+        added_row_set = set([row["id"] for row in added_rows])
+        added_col_set = set([col["id"] for col in added_columns])
+        structure_row_set.difference_update(removed_row_set)
+        structure_row_set.update(added_row_set)
+        structure_col_set.difference_update(removed_col_set)
+        structure_col_set.update(added_col_set)
+        final_rows = list(structure_row_set)
+        final_columns = list(structure_col_set)
+
+        added_cells = set()
+        for column in final_columns:
+            for row in list(added_row_set):
+                added_cells.add("{0}-{1}".format(row, column))
+        for row in final_rows:
+            for column in list(added_col_set):
+                added_cells.add("{0}-{1}".format(row, column))
+        for cell in list(added_cells):
+            try:
+                defaultcell = {
+                  "id": cell,
+                  "text": "",
+                  "bgcolor": "#F0F0F0",
+                  "color": "#969696",
+                  "animation": "none",
+                  "images": [],
+                  "notes": [],
+                  "boolean": "none"
+                }
+                write_json(root+"/data/"+cell+".json", defaultcell)
+            except:
+                pass
+        return working
+    else:
+        print("Errors were found with your update. No changes were made.")
+        return structure
+
+def cell_updater(cell, root, row_id=None, column_id=None, text=None, background_color=None, text_color=None, boolean=None, animation=None, add_image=None, remove_image=None, add_note=None):
+    working = copy.deepcopy(cell)
+    if text != None and type(text) == str:
+        working["text"] = str(text)
+    if background_color != None and is_hex_color(background_color):
+        working["bgcolor"] = background_color
+    if text_color != None and is_hex_color(text_color):
+        working["color"] = text_color
+    if boolean != None and boolean in [True, "True","true"]:
+        working["boolean"] = True
+    elif boolean != None and boolean in [False, "False","false"]:
+        working["boolean"] = False
+    elif boolean != None and boolean in ["None", "none"]:
+        working["boolean"] = None
+    if animation != None and animation in ["none", "wave", "toggle", "bars"]:
+        working["animation"] = animation
+    if add_note != None and type(add_note) == str:
+        print("adding note: " +add_note)
+        working["notes"].append(add_note)
+    if remove_image != None and type(int(remove_image)) == int:
+        try:
+            index = int(remove_image)
+            imagepath = root+"/"+working["images"][index]
+            os.unlink(imagepath)
+            working["images"].remove(working["images"][index])
+        except:
+            pass
+    if add_image != None and type(add_image) == str:
+        try:
+            filepath, filename = os.path.split(cleaned_path(add_image))
+            destination = root+"/images/"+filename
+            shutil.copyfile(cleaned_path(add_image), destination)
+            relpath = "images/"+filename
+            working["images"].append(relpath)
+        except:
+            pass
+    return working
+
+def create(dirpath, name):
+    src = get_dash_src()
+    root = cleaned_path(dirpath)
+    os.makedirs(root)
+    os.mkdir(root+"/data")
+    os.mkdir(root+"/images")
+    shutil.copytree(src+"/resources/assets", root+"/assets")
+    shutil.copytree(src+"/resources/templates", root+"/templates")
+    shutil.copyfile(src+"/resources/index.html", root+"/index.html")
+    startingdata = {"name": name, "rows": [], "cols": []}
+    write_json(root+"/data/structure.json", startingdata)
+
+def update(dirpath, add_columns=None, remove_columns=None, add_rows=None, remove_rows=None):
+    root = cleaned_path(dirpath)
+    update_json(root+"/data/structure.json", callback=structure_updater, root=root, add_columns=add_columns, remove_columns=remove_columns, add_rows=add_rows, remove_rows=remove_rows)
+
+def cell(dirpath, row_id=None, column_id=None, text=None, background_color=None, text_color=None, boolean=None, animation=None, add_image=None, remove_image=None, add_note=None):
+    root = cleaned_path(dirpath)
+    update_json(root+"/data/{0}-{1}.json".format(row_id, column_id), callback=cell_updater, root=root, row_id=row_id, column_id=column_id, text=text, background_color=background_color, text_color=text_color, boolean=boolean, animation=animation, add_image=add_image, remove_image=remove_image, add_note=add_note)
+
+def query(dirpath, row_id=None, column_id=None, field=None):
+    print()
+
+#============================================================================
+#       Main
+#============================================================================
+
+if __name__ == '__main__':
+    arguments = docopt(doc, version='Dash v{0}'.format(Version))
+    print(arguments)
+    if arguments["create"] == True:
+        create(arguments["<dir>"], arguments["--name"])
+    elif arguments["update"] == True:
+        update(arguments["<dir>"], add_columns=arguments["--addcol"],
+                                   remove_columns=arguments["--rmcol"],
+                                   add_rows=arguments["--addrow"],
+                                   remove_rows=arguments["--rmrow"])
+    elif arguments["cell"] == True:
+        cell(arguments["<dir>"], row_id=arguments["--row"],
+                                 column_id=arguments["--col"],
+                                 text=arguments["--settext"],
+                                 background_color=arguments["--setbgcolor"],
+                                 text_color=arguments["--settxtcolor"],
+                                 boolean=arguments["--setbool"],
+                                 animation=arguments["--setanimate"],
+                                 add_image=arguments["--addimage"],
+                                 remove_image=arguments["--rmimage"],
+                                 add_note=arguments["--addnote"])
---
+++ resources/assets/js/app.js
@@ -0,0 +1,118 @@
+$.ajaxSetup({ cache: false });
+var structure = {"name": "DASH",
+                 "rows": [],
+                 "columns": []
+                 }
+
+function loadjson(filepath, callback) {
+  console.log("Attempting to load "+filepath)
+  setTimeout(function() {
+    $.ajax({ url: filepath,
+      success: function(data) {
+        callback(data)
+      }, dataType: "json",
+      error: function(data) {
+        callback()
+        //console.log("Error!", data);
+      }
+    });
+  }, 100);
+}
+
+function addmodal(data) {
+  if (!data) {
+    data = {'ModalTitle': "Under Construction", 'ModalContent': 'None'}
+  };
+  $("#modalcontent").processTemplate(data);
+  $('#modalcontent').modal('show')
+}
+
+function addcontent(data) {
+  if (!data) {
+    console.error("404 Not Found")
+  } else {
+    templatecontents["contents"] = data
+    $("#bodycontent").processTemplate(templatecontents);
+    $('pre').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+  }
+}
+
+function loadmodal(id) {
+  console.log("Loading cell "+id);
+  $("#modalcontent").setTemplateURL("templates/modalcontent.html", { filter_data: false });
+  loadjson("data/" + id + ".json", addmodal);
+}
+
+function update_cell(data) {
+  if (!data) {
+    console.error("404 Not Found")
+  } else {
+    cell = document.getElementById(data.id)
+    cell.style.cssText = "color: "+data.color+"; background-color: "+data.bgcolor
+    cellclass = "statuscell"
+    if (data.animation != "none") {
+      cellclass += " statuscell-"+data.animation
+    }
+    cell.className = cellclass
+    cell.innerHTML = data.text
+  }
+}
+
+function generate(data) {
+  if (!data) {
+    console.error("404 Not Found");
+  } else {
+    if (JSON.stringify(data) !== JSON.stringify(structure) || data.rows.length == 0 || data.cols.length == 0) {
+      console.log("Updating structure")
+      titleElement = document.getElementById("title");
+      titleElement.innerHTML = "DASH | " + data["name"];
+      navtitleElement = document.getElementById("navbartitle");
+      navtitleElement.innerHTML = data["name"];
+      if (data.rows.length == 0 || data.cols.length == 0) {
+        noticeElement = document.getElementById('notice');
+        noticeElement.innerHTML = '<div class="alert alert-warning" role="alert">There is nothing to show! Add columns or rows to get started!</div>'
+        tableElement = document.getElementById('table');
+        tableElement.innerHTML = "";
+      } else {
+        noticeElement = document.getElementById('notice');
+        noticeElement.innerHTML = ''
+        table = '<tr><th id="column_headers" class="statuscell text-center darker"></th>';
+        for (c = 0; c < data.cols.length; c++) {
+          table = table += '<th class="statuscell text-center darker" title="ID: {%%COLUMN_ID%%}">{%%COLUMN_NAME%%}</th>'.replace("{%%COLUMN_ID%%}", data.cols[c].id).replace("{%%COLUMN_NAME%%}", data.cols[c].text)
+        }
+        table += "</tr>";
+        for (r = 0; r < data.rows.length; r++) {
+          table += '<tr><th class="statuscell text-center darker" title="ID: {%%ROW_ID%%}">{%%ROW_NAME%%}</th>'.replace("{%%ROW_ID%%}", data.rows[r].id).replace("{%%ROW_NAME%%}", data.rows[r].text)
+          for (c = 0; c < data.cols.length; c++) {
+            table += '<td class="statuscell" id="{%%ROW_ID%%}-{%%COLUMN_ID%%}" onclick="loadmodal(id)"></td>'.replace("{%%ROW_ID%%}", data.rows[r].id).replace("{%%COLUMN_ID%%}", data.cols[c].id)
+          }
+          table += "</tr>"
+        }
+        tableElement = document.getElementById('table');
+        tableElement.innerHTML = table;
+        structure = data
+      }
+    }
+
+    // Update cells
+    for (r = 0; r < data.rows.length; r++) {
+      for (c = 0; c < data.cols.length; c++) {
+        cellid = data.rows[r].id + "-" + data.cols[c].id;
+        loadjson("data/"+cellid+".json", update_cell)
+      }
+    }
+  }
+}
+
+function update_structure() {
+  loadjson("data/structure.json", generate)
+}
+
+function start() {
+  update_structure()
+  setInterval(function () {
+      update_structure()
+    }, 10000);
+}
